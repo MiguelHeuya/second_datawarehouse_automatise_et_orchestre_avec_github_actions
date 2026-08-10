6cc6b283848e99b6d138cf8e498cf3eb92106cb6
HSML-hsml-2222 <hsmlheuyaseugangmiguelleandre@gmail.com>
2026-08-10 12:51:49 +0100

    tout recommencer des le debut

---
+++ gold script.sql
@@ -0,0 +1,119 @@
+
+
+
+DROP TABLE IF EXISTS gold.fact_orders CASCADE;
+CREATE TABLE gold.fact_orders AS 
+(WITH avg_reviews_per_order AS (
+    SELECT 
+        order_id,
+        AVG(review_score) AS avg_score
+    FROM silver.sourceb_source_olist_order_reviews_dataset
+    GROUP BY order_id
+)
+SELECT
+    -- 1. IDENTIFIANTS & CLÉS
+    a.order_id                      AS order_id,
+    c.seller_id                     AS seller_id,
+    c.product_id                    AS product_id,
+    a.customer_id                   AS customer_id,
+
+	
+    -- 2. STATUT & SUIVI TEMPOREL
+    a.order_status                  AS order_status,
+    a.order_purchase_timestamp      AS order_purchase_date,
+    a.order_approved_at             AS order_approved_date,
+    a.order_delivered_carrier_date  AS carrier_delivery_date,
+    a.order_delivered_customer_date AS customer_delivery_date,
+    a.order_estimated_delivery_date AS estimated_delivery_date,
+    c.shipping_limit_date           AS seller_shipping_limit_date,
+
+    -- 3. ÉVALUATION ET SATISFACTION CLIENT
+    ROUND(
+        CAST(
+            COALESCE(
+                r.avg_score, 
+                (SELECT AVG(review_score) FROM silver.sourceb_source_olist_order_reviews_dataset)
+            ) AS numeric
+        ), 2
+    )                               AS customer_review_score,
+
+    -- 4. DÉTAILS DE L'ARTICLE & FRAIS ASSOCIÉS
+    c.price::numeric                         AS item_price,
+    c.freight_value::numeric                 AS item_freight_value,
+    ROUND(
+        CAST(b.total_payment_value - (c.price + c.freight_value) AS numeric), 
+        2
+    )                               AS installment_or_tip_fee,
+
+    -- 5. ÉCHÉANCIER, AJUSTEMENTS & MODES DE PAIEMENT
+    b.payment_sequential::numeric            AS payment_sequence_number,
+    b.payment_installments::numeric          AS payment_installments_count,
+    b.has_credit_card::numeric               AS is_paid_by_credit_card,
+    b.has_boleto::numeric                    AS is_paid_by_boleto,
+    b.has_voucher::numeric                   AS is_paid_by_voucher,
+    b.has_debit_card::numeric                AS is_paid_by_debit_card,
+    b.total_payment_value::numeric           AS total_payment_amount
+
+FROM silver.sourceb_source_olist_orders_dataset AS a
+INNER JOIN silver.sourcea_source_olist_order_payments_dataset AS b
+    ON a.order_id = b.order_id
+INNER JOIN silver.sourcea_source_olist_order_items_dataset AS c
+    ON a.order_id = c.order_id
+LEFT JOIN avg_reviews_per_order AS r
+    ON a.order_id = r.order_id
+LEFT JOIN silver.sourcea_source_olist_customers_dataset AS d
+	ON a.customer_id = d.customer_id
+);
+
+
+DROP TABLE IF EXISTS gold.dim_customers CASCADE;
+CREATE TABLE gold.dim_customers AS(
+	SELECT
+		customer_id,
+		customer_zip_code_prefix AS customer_zip_code,
+		customer_city,
+		customer_state
+	FROM silver.sourcea_source_olist_customers_dataset
+);
+
+DROP TABLE IF EXISTS gold.dim_sellers CASCADE;
+CREATE TABLE gold.dim_sellers AS(
+	SELECT
+		seller_id,
+		seller_zip_code_prefix AS seller_zip_code,
+		seller_city,
+		seller_state
+	FROM silver.sourceb_source_olist_sellers_dataset
+);
+
+DROP TABLE IF EXISTS gold.dim_products CASCADE;
+CREATE TABLE gold.dim_products AS
+	(SELECT
+	    -- 1. CLÉ PRIMAIRE & IDENTIFIANT
+	    p.product_id,
+	
+	    -- 2. CATÉGORIES (ORIGINALE ET TRADUITE)
+	    p.product_category_name,
+	    COALESCE(
+	        NULLIF(t.product_category_name_english, 'n/a'), 
+	        NULLIF(p.product_category_name, 'n/a'), 
+	        'non spécifié'
+	    ) AS product_category_name_english,
+	    -- 3. CARACTÉRISTIQUES DE LA FICHE PRODUIT
+	    COALESCE(p.product_name_lenght, 0)::numeric         AS product_name_length,
+	    COALESCE(p.product_description_lenght, 0)::numeric  AS product_description_length,
+	    COALESCE(p.product_photos_qty, 0)::numeric          AS product_photos_qty,
+	    -- 4. PAYSAGE PHYSIQUE & LOGISTIQUE
+	    p.product_weight_g::numeric,
+	    p.product_length_cm::numeric,
+	    p.product_height_cm::numeric,
+	    p.product_width_cm::numeric,
+	    -- Indicateur dérivé pour l'analyse logistique (Volume en cm3)
+	    ROUND(
+	        CAST(p.product_length_cm * p.product_height_cm * p.product_width_cm AS numeric), 
+	        2
+	    ) AS product_volume_cm3
+	
+	FROM silver.sourceb_source_olist_products_dataset AS p
+	LEFT JOIN silver.sourceb_source_product_category_name_translation AS t
+	    ON p.product_category_name = t.product_category_name);
---
+++ transformation script.sql
@@ -0,0 +1,209 @@
+
+
+DROP TABLE IF EXISTS silver.sourcea_source_olist_customers_dataset CASCADE;
+CREATE TABLE silver.sourcea_source_olist_customers_dataset AS (
+    SELECT
+        customer_id,
+        customer_unique_id,
+        customer_zip_code_prefix,
+        customer_city,
+        CASE customer_state
+            WHEN 'AC' THEN 'Acre'
+            WHEN 'AL' THEN 'Alagoas'
+            WHEN 'AM' THEN 'Amazonas'
+            WHEN 'AP' THEN 'Amapá'
+            WHEN 'BA' THEN 'Bahia'
+            WHEN 'CE' THEN 'Ceará'
+            WHEN 'DF' THEN 'Distrito Federal'
+            WHEN 'ES' THEN 'Espírito Santo'
+            WHEN 'GO' THEN 'Goiás'
+            WHEN 'MA' THEN 'Maranhão'
+            WHEN 'MG' THEN 'Minas Gerais'
+            WHEN 'MS' THEN 'Mato Grosso do Sul'
+            WHEN 'MT' THEN 'Mato Grosso'
+            WHEN 'PA' THEN 'Pará'
+            WHEN 'PB' THEN 'Paraíba'
+            WHEN 'PE' THEN 'Pernambuco'
+            WHEN 'PI' THEN 'Piauí'
+            WHEN 'PR' THEN 'Paraná'
+            WHEN 'RJ' THEN 'Rio de Janeiro'
+            WHEN 'RN' THEN 'Rio Grande do Norte'
+            WHEN 'RO' THEN 'Rondônia'
+            WHEN 'RR' THEN 'Roraima'
+            WHEN 'RS' THEN 'Rio Grande do Sul'
+            WHEN 'SC' THEN 'Santa Catarina'
+            WHEN 'SE' THEN 'Sergipe'
+            WHEN 'SP' THEN 'São Paulo'
+            WHEN 'TO' THEN 'Tocantins'
+            ELSE customer_state
+        END AS customer_state
+    FROM bronze.sourcea_source_olist_customers_dataset);
+
+
+DROP TABLE IF EXISTS silver.sourcea_source_olist_geolocation_dataset CASCADE;
+CREATE TABLE silver.sourcea_source_olist_geolocation_dataset AS (
+    SELECT DISTINCT
+        geolocation_zip_code_prefix,
+        geolocation_lat,
+        geolocation_lng,
+        geolocation_city,
+        CASE geolocation_state
+            WHEN 'AC' THEN 'Acre'
+            WHEN 'AL' THEN 'Alagoas'
+            WHEN 'AM' THEN 'Amazonas'
+            WHEN 'AP' THEN 'Amapá'
+            WHEN 'BA' THEN 'Bahia'
+            WHEN 'CE' THEN 'Ceará'
+            WHEN 'DF' THEN 'Distrito Federal'
+            WHEN 'ES' THEN 'Espírito Santo'
+            WHEN 'GO' THEN 'Goiás'
+            WHEN 'MA' THEN 'Maranhão'
+            WHEN 'MG' THEN 'Minas Gerais'
+            WHEN 'MS' THEN 'Mato Grosso do Sul'
+            WHEN 'MT' THEN 'Mato Grosso'
+            WHEN 'PA' THEN 'Pará'
+            WHEN 'PB' THEN 'Paraíba'
+            WHEN 'PE' THEN 'Pernambuco'
+            WHEN 'PI' THEN 'Piauí'
+            WHEN 'PR' THEN 'Paraná'
+            WHEN 'RJ' THEN 'Rio de Janeiro'
+            WHEN 'RN' THEN 'Rio Grande do Norte'
+            WHEN 'RO' THEN 'Rondônia'
+            WHEN 'RR' THEN 'Roraima'
+            WHEN 'RS' THEN 'Rio Grande do Sul'
+            WHEN 'SC' THEN 'Santa Catarina'
+            WHEN 'SE' THEN 'Sergipe'
+            WHEN 'SP' THEN 'São Paulo'
+            WHEN 'TO' THEN 'Tocantins'
+            ELSE geolocation_state
+        END AS geolocation_state
+    FROM bronze.sourcea_source_olist_geolocation_dataset
+);
+
+
+DROP TABLE IF EXISTS silver.sourcea_source_olist_order_items_dataset;
+CREATE TABLE silver.sourcea_source_olist_order_items_dataset AS
+(SELECT
+	order_id,
+	MIN(order_item_id) AS order_item_id,
+	MAX(product_id) AS product_id,
+	MAX(seller_id) AS seller_id,
+	MAX(shipping_limit_date::date) AS shipping_limit_date,
+	SUM(price::double precision) AS price,
+	SUM(freight_value::double precision) AS freight_value
+FROM bronze.sourcea_source_olist_order_items_dataset
+GROUP BY order_id
+);
+
+DROP TABLE IF EXISTS silver.sourcea_source_olist_order_payments_dataset;
+CREATE TABLE silver.sourcea_source_olist_order_payments_dataset AS(
+SELECT
+    order_id,
+    SUM(payment_sequential::double precision) AS payment_sequential,
+    SUM(payment_installments::double precision) AS payment_installments,
+    COALESCE(SUM(CASE WHEN payment_type = 'credit_card' THEN payment_value::double precision ELSE 0 END), 0) AS has_credit_card,
+    COALESCE(SUM(CASE WHEN payment_type = 'boleto' THEN payment_value::double precision ELSE 0 END), 0) AS has_boleto,
+    COALESCE(SUM(CASE WHEN payment_type = 'voucher' THEN payment_value::double precision ELSE 0 END), 0) AS has_voucher,
+    COALESCE(SUM(CASE WHEN payment_type = 'debit_card' THEN payment_value::double precision ELSE 0 END), 0) AS has_debit_card,
+	SUM(payment_value::double precision) AS total_payment_value
+FROM bronze.sourcea_source_olist_order_payments_dataset
+GROUP BY order_id
+);
+
+
+DROP TABLE IF EXISTS silver.sourceb_source_olist_order_reviews_dataset;
+CREATE TABLE silver.sourceb_source_olist_order_reviews_dataset AS
+	(
+	SELECT
+		review_id,
+		order_id,
+		review_score:: double precision,
+		COALESCE(review_comment_title, 'n/a') AS review_comment_title,
+		COALESCE(review_comment_message, 'n/a') AS review_comment_message,
+		review_creation_date::date,
+		review_answer_timestamp::date
+	FROM bronze.sourceb_source_olist_order_reviews_dataset
+);
+
+
+DROP TABLE IF EXISTS silver.sourceb_source_olist_orders_dataset;
+CREATE TABLE silver.sourceb_source_olist_orders_dataset AS
+(SELECT
+	order_id,
+	customer_id,
+	order_status,
+	order_purchase_timestamp::date,
+	order_approved_at::date,
+	order_delivered_carrier_date::date,
+	order_delivered_customer_date::date,
+	order_estimated_delivery_date::date
+FROM bronze.sourceb_source_olist_orders_dataset
+);
+
+
+DROP TABLE IF EXISTS silver.sourceb_source_olist_products_dataset;
+CREATE TABLE silver.sourceb_source_olist_products_dataset AS
+(
+	SELECT
+		product_id,
+		COALESCE(product_category_name, 'n/a') AS product_category_name,
+		product_name_lenght::double precision,
+		product_description_lenght::double precision,
+		product_photos_qty::double precision,
+		product_weight_g::double precision,
+		product_length_cm::double precision,
+		product_height_cm::double precision,
+		product_width_cm::double precision
+	FROM bronze.sourceb_source_olist_products_dataset
+);
+
+DROP TABLE IF EXISTS silver.sourceb_source_olist_sellers_dataset CASCADE;
+CREATE TABLE silver.sourceb_source_olist_sellers_dataset AS (
+    SELECT
+        seller_id,
+        seller_zip_code_prefix,
+        seller_city,
+        CASE seller_state
+            WHEN 'AC' THEN 'Acre'
+            WHEN 'AL' THEN 'Alagoas'
+            WHEN 'AM' THEN 'Amazonas'
+            WHEN 'AP' THEN 'Amapá'
+            WHEN 'BA' THEN 'Bahia'
+            WHEN 'CE' THEN 'Ceará'
+            WHEN 'DF' THEN 'Distrito Federal'
+            WHEN 'ES' THEN 'Espírito Santo'
+            WHEN 'GO' THEN 'Goiás'
+            WHEN 'MA' THEN 'Maranhão'
+            WHEN 'MG' THEN 'Minas Gerais'
+            WHEN 'MS' THEN 'Mato Grosso do Sul'
+            WHEN 'MT' THEN 'Mato Grosso'
+            WHEN 'PA' THEN 'Pará'
+            WHEN 'PB' THEN 'Paraíba'
+            WHEN 'PE' THEN 'Pernambuco'
+            WHEN 'PI' THEN 'Piauí'
+            WHEN 'PR' THEN 'Paraná'
+            WHEN 'RJ' THEN 'Rio de Janeiro'
+            WHEN 'RN' THEN 'Rio Grande do Norte'
+            WHEN 'RO' THEN 'Rondônia'
+            WHEN 'RR' THEN 'Roraima'
+            WHEN 'RS' THEN 'Rio Grande do Sul'
+            WHEN 'SC' THEN 'Santa Catarina'
+            WHEN 'SE' THEN 'Sergipe'
+            WHEN 'SP' THEN 'São Paulo'
+            WHEN 'TO' THEN 'Tocantins'
+            ELSE seller_state
+        END AS seller_state
+    FROM bronze.sourceb_source_olist_sellers_dataset
+);
+
+
+DROP TABLE IF EXISTS silver.sourceb_source_product_category_name_translation CASCADE;
+CREATE TABLE silver.sourceb_source_product_category_name_translation AS (
+    SELECT
+        COALESCE("ï»¿product_category_name", 'n/a') AS "product_category_name",
+        COALESCE(product_category_name_english, 'n/a') AS product_category_name_english
+    FROM bronze.sourceb_source_product_category_name_translation
+);
+
+
+
