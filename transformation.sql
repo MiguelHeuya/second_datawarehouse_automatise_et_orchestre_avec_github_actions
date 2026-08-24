3148a4c8d157cd2f933a9438aac052659c406b6a
HSML-hsml-2222 <hsmlheuyaseugangmiguelleandre@gmail.com>
2026-08-24 18:30:41 +0100

    transformation.sql

---
+++ transformation.sql
@@ -0,0 +1,19 @@
+
+
+
+
+
+SELECT
+	*
+FROM bronze.crm_cust_info
+WHERE cst_id = '29473'
+
+SELECT
+    *
+FROM bronze.erp_cust_az12
+WHERE "CID" = 'AW00029466';
+
+SELECT
+	*
+FROM bronze.erp_loc_a101
+WHERE "CID" LIKE '%29466'
